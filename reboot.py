fc7aac0b7498a62ac5a8f8412c6b3b0c958d1b9e
huntyGwell <108026570+huntyGwell@users.noreply.github.com>
2024-06-24 17:05:30 -0600

    Create reboot.py

---
+++ reboot.py
@@ -0,0 +1,5 @@
+import os
+#wildly simple
+os.system("reboot")
+# initally I was trying to use 'sudo' and pipe, cat, or echo, the sudo password in but if you omit the
+# sudo it will reboot automatically
